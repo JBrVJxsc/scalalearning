521e5facf547b85fec53d015eb3be8bf16e1b8d6
laonawuli <laonawuli@126.com>
2014-07-07 11:16:51 +0800

    Code

--- src/com/scala/exercises/impatient/chapter4/Exercise4.scala
+++ src/com/scala/exercises/impatient/chapter4/Exercise4.scala
@@ -1,7 +1,11 @@
 package com.scala.exercises.impatient.chapter4
 
+import java.util
+
 import com.scala.exercises.ScalaExercise
 import com.scala.interfaces.Exercise
+
+import scala.collection.mutable
 
 /**
  * Created by Who on 2014/7/6.
@@ -64,7 +68,133 @@ class Exercise4 extends ScalaExercise with Exercise {
 
   addQ(
     () => {
+      val strs = Array[String]("A", "B", "A", "B", "A", "B", "C", "B", "C", "B", "D", "B", "D", "B")
+      var map = Map[String, Int]()
+      for (i <- strs) {
+        if (map.contains(i)) {
+          map = map + (i -> (map(i) + 1))
+        }
+        else {
+          map = map + (i -> 1)
+        }
+      }
+      print(map)
+    }
+  )
 
+  addQ(
+    () => {
+      val strs = Array[String]("A", "B", "A", "B", "A", "B", "C", "B", "C", "B", "D", "B", "D", "B")
+      var map = scala.collection.immutable.SortedMap[String, Int]()
+      for (i <- strs) {
+        if (map.contains(i)) {
+          map = map + (i -> (map(i) + 1))
+        }
+        else {
+          map = map + (i -> 1)
+        }
+      }
+      print(map)
+    }
+  )
+
+  addQ(
+    () => {
+      val strs = Array[String]("A", "B", "A", "B", "A", "B", "C", "B", "C", "B", "D", "B", "D", "B")
+      val map = new util.TreeMap[String, Int]
+      for (i <- strs) {
+        if (map.containsKey(i)) {
+          map.put(i, map.get(i) + 1)
+        }
+        else {
+          map.put(i, 1)
+        }
+      }
+      print(map)
+    }
+  )
+
+  addQ(
+    () => {
+      val strs = Array[String]("A", "B", "A", "B", "A", "B", "C", "B", "C", "B", "D", "B", "D", "B")
+      val map = new mutable.LinkedHashMap[String, Int]
+      map.put("Monday", java.util.Calendar.MONDAY)
+      map.put("Tuesday", java.util.Calendar.TUESDAY)
+      map.put("Wednesday", java.util.Calendar.WEDNESDAY)
+      map.put("Thursday", java.util.Calendar.THURSDAY)
+      map.put("Friday", java.util.Calendar.FRIDAY)
+      map.put("Saturday", java.util.Calendar.SATURDAY)
+      map.put("Sunday", java.util.Calendar.SUNDAY)
+      print(map)
+    }
+  )
+
+  addQ(
+    () => {
+      val map = new mutable.LinkedHashMap[String, String]
+      map.put("java.rutime.name", "Java (TM) SE Runtime Environment")
+      map.put("sun.boot.library.path", "/home/apps/jdk1.6.0_21/jre/lib/i386")
+      map.put("java.vm.version", "17.0-b16")
+      map.put("java.vm.vendor", "Sun Microsystems Inc.")
+      map.put("java.vendor.url", "http://java.sun.com/")
+      map.put("path.separator", ":")
+      map.put("java.vm.name", "Java HotSpot (TM) Server VM")
+      var max = 0
+      for ((k, v) <- map) {
+        if (k.length > max) {
+          max = k.length
+        }
+      }
+      max += 5
+      for ((k, v) <- map) {
+        print(fix(k) + " | " + v)
+      }
+
+      def fix(s: String): String = {
+        s + " " * (max - s.length)
+      }
+    }
+  )
+
+  addQ(
+    () => {
+      val a = Array[Int](1, 2, -3, 4, 50, 6)
+      print(minmax(a))
+
+      def minmax(values: Array[Int]): Tuple2[Int, Int] = {
+        (values.min, values.max)
+      }
+    }
+  )
+
+  addQ(
+    () => {
+      val a = Array[Int](1, 2, -3, 4, 50, 6)
+      print(lteqgt(a,2))
+
+      def lteqgt(values: Array[Int], v: Int) = {
+        var less = 0
+        var eq = 0
+        var big = 0
+        for (i <- values) {
+          if (i < v) {
+            less += 1
+          }
+          else if (i == v) {
+            eq += 1
+          }
+          else {
+            big += 1
+          }
+        }
+        (less, eq, big)
+      }
+    }
+  )
+
+  addQ(
+    () => {
+      print("Hello".zip("World"))
     }
   )
 }
---
+++ src/com/scala/exercises/impatient/chapter5/Exercise5.scala
@@ -0,0 +1,13 @@
+package com.scala.exercises.impatient.chapter5
+
+import com.scala.exercises.ScalaExercise
+import com.scala.interfaces.Exercise
+
+/**
+ * Created by Who on 2014/7/7.
+ */
+class Exercise5 extends ScalaExercise with Exercise {
+  override def getName: String = "Chapter5"
+
+
+}
