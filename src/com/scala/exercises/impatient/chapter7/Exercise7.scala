facb4e94f522bc77b43fbe8f12355a53979304a3
Xu ZHANG <laonawuli@126.com>
2014-07-09 23:24:10 +0800

    Code

---
+++ src/com/scala/exercises/impatient/chapter7/Exercise7.scala
@@ -0,0 +1,13 @@
+package com.scala.exercises.impatient.chapter7
+
+import com.scala.exercises.ScalaExercise
+import com.scala.interfaces.Exercise
+
+/**
+ * Created by Who on 14-7-9.
+ */
+class Exercise7 extends ScalaExercise with Exercise {
+  override def getName: String = "Chapter7"
+
+
+}
