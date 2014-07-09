1f1585f3efdea070f5e6a59c208e0552241dfb24
Xu ZHANG <laonawuli@126.com>
2014-07-09 12:30:24 +0800

    Code

--- src/com/scala/exercises/impatient/chapter5/Exercise5.scala
+++ src/com/scala/exercises/impatient/chapter5/Exercise5.scala
@@ -58,4 +58,79 @@ class Exercise5 extends ScalaExercise with Exercise {
       }
     }
   )
+
+  addQ(
+    () => {
+
+      class BankAccount {
+        private var balance = 0d
+
+        def deposit(value: Double): Double = {
+          if (value > 0) {
+            balance += value
+          }
+          balance
+        }
+
+        def withdraw(value: Double): Double = {
+          if (value <= balance) {
+            balance -= value
+          }
+
+          balance
+        }
+      }
+
+      val bankAccount = new BankAccount
+      println(bankAccount.deposit(1000))
+      println(bankAccount.withdraw(499))
+    }
+  )
+
+  addQ(
+    () => {
+
+      class Time {
+        private var hours = 0
+        private var minutes = 0
+
+
+        def this(hours: Int, minutes: Int) {
+          this()
+          if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+            throw new Exception("Wrong format of the Time.")
+          }
+          this.hours = hours
+          this.minutes = minutes
+        }
+
+        def before(other: Time): Boolean = {
+          if (hours < other.hours) {
+            true
+          }
+          else if (hours > other.hours) {
+            false
+          }
+          else {
+            if (minutes < other.minutes) {
+              true
+            }
+            else {
+              false
+            }
+          }
+        }
+      }
+
+
+      val time1 = new Time(22, 22)
+      val time2 = new Time(21, 22)
+      val time3 = new Time(22, 21)
+      println(time1.before(time2))
+      println(time1.before(time3))
+      println(time2.before(time1))
+      println(time3.before(time1))
+      println(time2.before(time3))
+    }
+  )
 }
--- src/com/scala/exercises/impatient/chapter5/Person.scala
+++ src/com/scala/exercises/impatient/chapter5/Person.scala
@@ -7,7 +7,8 @@ class Person(var nameN: String, var ageN: Int = 20) {
   private var name = ""
   private var privateAge = 0
 
-  // Because the class has provided the constructor (behind the class name), so there is no place for the constructors below.
+  // Because the class has provided the constructor (behind the class name), so there is no place for the constructors below, unless you got
+  // different constructors like with different parameters.
 //  def this(name: String) {
 //    this()
 //    this.name = name
