202ce719cdcbbf3e5af67380a20cd36d5747b742
Xu ZHANG <laonawuli@126.com>
2014-07-10 12:47:48 +0800

    Code

---
+++ src/com/scala/exercises/impatient/chapter7/Employee.scala
@@ -0,0 +1,17 @@
+package com.scala.exercises.impatient.chapter7
+
+/**
+ * Created by Who on 14-7-10.
+ */
+
+package com {
+  package horstmann {
+    package impatient {
+      class Employee {
+
+      }
+    }
+  }
+}
+
+
