12bc14b30eabaa33cb06292319261a948127d732
Xu ZHANG <laonawuli@126.com>
2014-07-11 14:02:07 +0800

    Code

--- src/com/scala/exercises/impatient/chapter9/Exercise9.scala
+++ src/com/scala/exercises/impatient/chapter9/Exercise9.scala
@@ -3,11 +3,30 @@ package com.scala.exercises.impatient.chapter9
 import com.scala.exercises.ScalaExercise
 import com.scala.interfaces.Exercise
 
+import scala.io.Source
+
 /**
  * Created by Who on 14-7-11.
  */
-class Exercise9 extends ScalaExercise with Exercise{
+class Exercise9 extends ScalaExercise with Exercise {
   override def getName: String = "Exercise9"
 
+  addT(
+    () => {
+      val fileName = getClass.getResource("myfile.txt").getPath
+      val source = Source.fromFile(fileName)
+      val lineIterator = source.getLines()
+      for (i <- lineIterator) {
+        print(i)
+      }
 
+      val lines = source.getLines().toArray
+      print(lines)
+
+      val contents = source.mkString
+      print(contents)
+
+      source.close()
+    }
+  )
 }
