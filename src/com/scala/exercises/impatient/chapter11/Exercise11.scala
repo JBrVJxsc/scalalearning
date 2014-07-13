d894904933135c096fd245072760a392bcce384f
Xu ZHANG <laonawuli@126.com>
2014-07-13 22:33:44 +0800

    Code

--- src/com/scala/exercises/impatient/chapter11/Exercise11.scala
+++ src/com/scala/exercises/impatient/chapter11/Exercise11.scala
@@ -190,6 +190,23 @@ class Exercise11 extends ScalaExercise with Exercise {
 
         val mat = Array.ofDim[Int](row, col)
 
+        def set(value: Int) = {
+          for (i <- 0 until row; j <- 0 until col) {
+            mat(i)(j) = value
+          }
+        }
+
+        def print() = {
+          println("")
+          for (i <- 0 until row) {
+            var str = ""
+            for (j <- 0 until col) {
+              str += mat(i)(j).toString + " "
+            }
+            println(str)
+          }
+        }
+
         def apply(row: Int)(col: Int) = {
           mat(row)(col)
         }
@@ -206,24 +223,98 @@ class Exercise11 extends ScalaExercise with Exercise {
 
           val row = mat.length
           val col = mat(0).length
-          val newMat = Array.ofDim[Int](row, col)
+          val newMat = new Matrix(row, col)
 
           for (i <- 0 until row; j <- 0 until col) {
-            newMat(i)(j) = mat(i)(j) + other(i)(j)
+            newMat(i, j) = mat(i)(j) + other(i)(j)
+          }
+          newMat
+        }
+
+        def *(other: Matrix) = {
+          if (mat(0).length != other.mat.length) {
+            throw new Exception("Wrong operation.")
+          }
+
+          val row = mat.length
+          val oldRow = mat(0).length
+          val col = other.mat(0).length
+          val newMat = new Matrix(row, col)
+
+          for (i <- 0 until row; j <- 0 until col) {
+            var sum = 0
+            for (c <- 0 until oldRow) {
+              sum += mat(i)(c) * other.mat(c)(j)
+            }
+
+            newMat(i, j) = sum
           }
           newMat
         }
       }
 
       val mat1 = new Matrix
-      mat1(0, 0) = 3
-      mat1(0, 1) = 3
-      mat1(1, 0) = 3
-      mat1(1, 1) = 3
+      mat1.set(3)
+      mat1.print
       val mat2 = new Matrix(2, 2)
       val mat3 = mat1 + mat2
       println("\n" + mat1(0)(0))
       println(mat3(0)(0))
+
+      val mat4 = new Matrix(2, 5)
+      mat4.set(5)
+      val mat5 = mat1 * mat4
+      mat5.print
+    }
+  )
+
+  addQ(
+    () => {
+      class RichFile {
+
+      }
+
+      object RichFile {
+
+        def unapply(filePath: String) = {
+          if (!filePath.contains(".") || !filePath.contains("/")) {
+            None
+          }
+          else {
+            val path = filePath.substring(0, filePath.lastIndexOf("/"))
+            val name = filePath.substring(filePath.lastIndexOf("/") + 1, filePath.lastIndexOf("."))
+            val extend = filePath.substring(filePath.lastIndexOf("."), filePath.length)
+            Some((path, name, extend))
+          }
+        }
+      }
+
+      val RichFile(path, name, extend) = "/home/cay/readme.txt"
+      println()
+      println(path)
+      println(name)
+      println(extend)
+    }
+  )
+
+  addQ(
+    () => {
+      class RichFile {
+
+      }
+
+      object RichFile {
+        def unapplySeq(filePath: String): Option[Seq[String]] = {
+          Some(filePath.split("/"))
+        }
+      }
+
+      val RichFile(nil, path, name, extend) = "/home/cay/readme.txt"
+      println()
+      println(nil)
+      println(path)
+      println(name)
+      println(extend)
     }
   )
 }
