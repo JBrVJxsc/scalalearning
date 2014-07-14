432bc12609564842a058acda8d086088161c22a5
Xu ZHANG <laonawuli@126.com>
2014-07-14 10:23:22 +0800

    Code

--- src/com/scala/exercises/impatient/chapter12/Exercise12.scala
+++ src/com/scala/exercises/impatient/chapter12/Exercise12.scala
@@ -37,4 +37,55 @@ class Exercise12 extends ScalaExercise with Exercise {
   addT(
     println((1 to 19).map(_.toString))
   )
+
+  addT(
+    () => {
+      print((1 to 9).reduceLeft(_ * _))
+    }
+  )
+
+  addT(
+    () => {
+      print("Mary has a little lamb".split(" ").sortWith(_.length < _.length))
+    }
+  )
+
+  addT(
+    () => {
+      def mul(x: Int, y: Int) = {
+        x * y
+      }
+
+      def mulOneAtATime(x: Int) = {
+        (y: Int) => x * y
+      }
+
+      print(mul(2, 3))
+
+      print(mulOneAtATime(2)(3))
+    }
+  )
+
+  addT(
+    () => {
+      def mulOneAtATime(x: Int)(y: Int) = {
+        x * y
+      }
+    }
+  )
+
+  addT(
+    () => {
+      val a = Array("1", "2")
+      val b = Array("hello", "world")
+      val c = Array(1, "2", true)
+      val d = Array(1, 2)
+      var result = a.corresponds(b)(_.equalsIgnoreCase(_))
+      print(result)
+      result = a.corresponds(d)(_==_)
+      print(result)
+    }
+  )
+
+
 }
