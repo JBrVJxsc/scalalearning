b4d1458b055f142cb8f8e03396f5b0f5dc163a48
Xu ZHANG <laonawuli@126.com>
2014-07-14 14:14:41 +0800

    Code

--- src/com/scala/exercises/impatient/chapter12/Exercise12.scala
+++ src/com/scala/exercises/impatient/chapter12/Exercise12.scala
@@ -163,7 +163,84 @@ class Exercise12 extends ScalaExercise with Exercise {
 
   addQ(
     () => {
+      val array = Array[Int](1, 2, 3, 4, 5)
+      val result = array.foldLeft(1)(_ * _)
+      print(result)
+    }
+  )
 
+  addQ(
+    () => {
+      def largest(fun: Int => Int, inputs: Seq[Int]) = {
+        fun(inputs.reduceLeft((x, y) => if (fun(x) > fun(y)) x else y))
+      }
+
+      val result = largest(x => 10 * x - x * x, 1 to 10)
+      print(result)
+    }
+  )
+
+  addQ(
+    () => {
+      def largest(fun: Int => Int, inputs: Seq[Int]) = {
+        inputs.reduceLeft((x, y) => if (fun(x) > fun(y)) x else y)
+      }
+
+      val result = largest(x => 10 * x - x * x, 1 to 10)
+      print(result)
+    }
+  )
+
+  addQ(
+    () => {
+      val pairs = (1 to 10) zip (11 to 20)
+      print(pairs)
+
+      def adjustToPairOld(fun: (Int, Int) => Int) = {
+        def opPair(pair: Tuple2[Int, Int]): Int = {
+          fun(pair._1, pair._2)
+        }
+
+        opPair _
+      }
+
+      def adjustToPair(fun: (Int, Int) => Int)(pair: Tuple2[Int, Int]) = {
+        fun(pair._1, pair._2)
+      }
+
+      print(adjustToPair(_ * _)((6, 7)))
+      var mapped = pairs.map(adjustToPair(_ + _))
+      print(mapped)
+      // Another easier way to do this.
+      mapped = pairs.map((x: Tuple2[Int, Int]) => x._1 + x._2)
+      print(mapped)
+    }
+  )
+
+  addQ(
+    () => {
+      val strArray = Array[String]("a", "bc", "def", "ghij", "klmno")
+      val intArray = Array[Int](1, 2, 3, 4, 5)
+      print(strArray.corresponds(intArray)(_.length == _))
+    }
+  )
+
+  addQ()
+
+  addQ(
+    () => {
+      def unless(condition: => Boolean)(block: => Unit) {
+        if (!condition) {
+          block
+          unless(condition)(block)
+        }
+      }
+
+      var count = 10
+      unless(count == 0) {
+        print(count)
+        count -= 1
+      }
     }
   )
 }
