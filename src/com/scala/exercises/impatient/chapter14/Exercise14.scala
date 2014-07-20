cd03114135f97b2ee80663878af2e1951feca09c
laonawuli <laonawuli@126.com>
2014-07-20 13:24:32 +0800

    Code

---
+++ src/com/scala/exercises/impatient/chapter14/Exercise14.scala
@@ -0,0 +1,13 @@
+package com.scala.exercises.impatient.chapter14
+
+import com.scala.exercises.ScalaExercise
+import com.scala.interfaces.Exercise
+
+/**
+ * Created by Who on 2014/7/20.
+ */
+class Exercise14 extends ScalaExercise with Exercise {
+  override def getName: String = "Chapter14"
+
+
+}
